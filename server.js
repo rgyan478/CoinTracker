2e362e2e5106239231d23fd28b78192e5f67c663
unknown <rgyan478@gmail.com>
2018-08-01 15:48:28 +0530

    color animation

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ MongoClient.connect(config.ServerConnectionURL, { useNewUrlParser: true } , func
 });//End Connection
 });//End Cron
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
 });
