31fbe33ea4d2556270c8f17fd2f3cf249e5ca2e1
unknown <rgyan478@gmail.com>
2018-08-01 22:48:37 +0530

    change Column name

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 //server connection
 mongoose.connect('mongodb://rgyan:rgyan123@ds245901.mlab.com:45901/currencytracker');
-// mongoose.connect('mongodb://localhost:27017/crypto_moon_tracker');
+ //mongoose.connect('mongodb://localhost:27017/crypto_moon_tracker');
 var db = mongoose.connection;
 var routes = require('./routes/index');
 var users = require('./routes/users');
