cc6fbb5bccb2e3681d88526b4e9899bbeaa3bb1d
unknown <rgyan478@gmail.com>
2018-08-01 21:25:25 +0530

    ]\

--- models/config.js
+++ models/config.js
@@ -1,3 +1,4 @@
+//module.exports.ServerConnectionURL='mongodb://localhost:27017/crypto_moon_tracker';
 module.exports.ServerConnectionURL='mongodb://rgyan:rgyan123@ds245901.mlab.com:45901/currencytracker';
 module.exports.LocalConnectionURL='mongodb://localhost:27017/currencytracker';
 module.exports.CurrencyApiCode=["BTC","ETH","LTC","USDT","BNB","USD","CAD","EUR","GBP","AUD","JPY","CNY","KRW","VND"];
